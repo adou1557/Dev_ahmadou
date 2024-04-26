f796aff906e4923097104b6b86137075730bd2f9
Your Name <you@example.com>
2024-04-26 22:31:36 +0100

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dev_ahmadou.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
